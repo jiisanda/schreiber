0e9c5590f7450329455569ed700806ebe9676328
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-08-26 17:55:56 +0530

    keys instead

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,31 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-
 fn die(e: std::io::Error) {
-    panic!(e);
+    panic!("{}", e);
 }
 
 
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
+    for key in io::stdin().keys() {
 
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?} \r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
                 }
-
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
-            }
+                Key::Ctrl('x') => break,
+                _ => println!("{:?}\r", key),
+            },
             Err(err) => die(err),
         }
     }
