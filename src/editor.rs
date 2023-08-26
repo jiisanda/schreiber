8a68b0341f3689144558a3bb279a633ede0c0270
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-08-26 19:10:08 +0530

    clear screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw:: IntoRawMode;
@@ -14,17 +14,29 @@ impl Editor {
         let _stdout = stdout().into_raw_mode().unwrap();
 
         loop {
-            if let Err(error) = self.process_keypress() {
+            if let Err(error) = self.refresh_screen() {
                 die(&error);
             }
             if self.should_quit {
                 break;
+            }
+            if let Err(error) = self.process_keypress() {
+                die(&error);
             }
         }
     }
 
     pub fn default() -> Self {
         Self { should_quit: false }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        // print!("\x1b[2J");
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        if self.should_quit {
+            println!("Goodbye. \r");
+        }
+        io::stdout().flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
@@ -47,5 +59,6 @@ fn read_key() -> Result<Key, std::io::Error> {
 }
 
 fn die(e: &std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{e}");
 }
