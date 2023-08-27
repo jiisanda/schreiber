b42810777d95cf16cc078f05ec99940bd8c78095
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-08-27 22:30:57 +0530

    welcome msg and changes

--- src/editor.rs
+++ src/editor.rs
@@ -1,17 +1,18 @@
-use std::io::{self, stdout, Write};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw:: IntoRawMode;
+use std::os::windows;
 
+use crate::Terminal;
+use termion::event::Key;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
 }
 
 
 impl Editor {
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
 
         loop {
             if let Err(error) = self.refresh_screen() {
@@ -27,38 +28,55 @@ impl Editor {
     }
 
     pub fn default() -> Self {
-        Self { should_quit: false }
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+        }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         // print!("\x1b[2J");
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        // print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        Terminal::cursor_hide();
+        Terminal::cursor_position(0, 0);
         if self.should_quit {
+            Terminal::clear_screen();
             println!("Goodbye. \r");
+        } else {
+            self.draw_rows();
+            // print!("{}", termion::cursor::Goto(1, 1));
+            Terminal::cursor_position(0, 0);
         }
-        io::stdout().flush()
+        Terminal::cursor_show();
+        Terminal::flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('x') => self.should_quit = true,
             _ => (),
         }
         Ok(())
     }
-}
 
-
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
+    fn draw_rows(&self) {
+        // for _ in 0..self.terminal.size().height - 1  {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row  == height / 3 {
+                let welcome_message = format!("Schreiber editor -- version {}", VERSION);
+                let width = std::cmp::min(self.terminal.size().width as usize, welcome_message.len());
+                println!("{}\r", &welcome_message[..width])
+            } else {
+                println!("~\r");
+            }
         }
     }
 }
 
 fn die(e: &std::io::Error) {
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!("{e}");
 }
