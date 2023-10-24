0ce84f9d9feb1df9c5eb9343c34b11cfad9c49b4
jiisanda <harshjaiswal2307@gmail.com>
2023-10-24 22:35:31 +0530

    main.rs update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod editor;
 mod terminal;
+mod document;
+mod row;
 
+pub use document::Document;
 use editor::Editor;
-pub use terminal::Terminal;
 pub use editor::Position;
+pub use row::Row;
+pub use terminal::Terminal;
 
 fn main() {
     Editor::default().run();
